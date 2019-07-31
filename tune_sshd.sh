52c6a030f5d9d780c14f2138857eb3f652c8e8fe
Blake Garner <bgarner@adobe.com>
2019-07-30 19:57:27 -0700

    remove include directive as it doesn't work as expected

--- tune_sshd.sh
+++ tune_sshd.sh
@@ -18,23 +18,6 @@ sudo sed -i '' 's/^ChallengeResponseAuthentication.*/ChallengeResponseAuthentica
 sudo sed -i '' 's/#PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sudo sed -i '' 's/^PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
-# create host specific path
-SSH_INCLUDE="/etc/ssh/sshd_config_$(hostname -s)"
-
-# setup the extra config file
-if [[ ! -e $SSH_INCLUDE ]]; then
-  echo "Creating host specific sshd config file $SSH_INCLUDE"
-  sudo touch $SSH_INCLUDE
-  sudo chown root:wheel $SSH_INCLUDE
-  sudo chmod 600 $SSH_INCLUDE
-fi
-
-# include extra config file that sshd can import if it's a new enough version
-if [[ -e $SSH_INCLUDE ]]; then
-  sudo sed -i '' '/Include/d' /etc/ssh/sshd_config
-  echo "Include $SSH_INCLUDE" | sudo tee -a /etc/ssh/sshd_config
-fi
-
 # Restart sshd to pickup config changes
 launchctl stop /System/Library/LaunchDaemons/ssh.plist || true
 launchctl start /System/Library/LaunchDaemons/ssh.plist || true
