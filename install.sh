99f3527215dfd5c6da9ea1352ce3b0aa5040bdc8
Blake Garner <@trodemaster>
2020-02-04 17:56:34 -0800

    install tweak

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@ IFS=$'\n\t'
 shopt -s nullglob 
 shopt -s nocaseglob
 
+# create /usr/local/bin if it doesn't exist
+if ! [[ -d /usr/local/bin ]]; then
+    sudo mkdir -p /usr/local/bin
+fi
+
 # move script into place
 sudo cp tune_sshd.sh /usr/local/bin/tune_sshd.sh
 sudo chmod +x /usr/local/bin/tune_sshd.sh
