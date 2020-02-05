7cbe25a6c0a99ced2c3846cbe56225edcc0b689f
Blake Garner <@trodemaster>
2020-02-04 22:29:15 -0800

    updating plist

--- install.sh
+++ install.sh
@@ -15,10 +15,12 @@ sudo chmod +x /usr/local/bin/tune_sshd.sh
 
 # move launchd plist into place
 sudo cp tune_sshd.plist /Library/LaunchDaemons/tune_sshd.plist
+sudo chown root:wheel /Library/LaunchDaemons/tune_sshd.plist
 
 # Stop start the new launchd item to load it
-launchctl stop /Library/LaunchDaemons/tune_sshd.plist || true
-launchctl start /Library/LaunchDaemons/tune_sshd.plist || true
+sudo launchctl load -w /Library/LaunchDaemons/tune_sshd.plist || true
+#launchctl stop /Library/LaunchDaemons/tune_sshd.plist || true
+#launchctl start /Library/LaunchDaemons/tune_sshd.plist || true
 
 # run the script
 sudo /usr/local/bin/tune_sshd.sh
